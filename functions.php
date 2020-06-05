fb1e7300d7a4231fa8d9a79bf2e248c2719457b8
ayako-1731 <ayakokyoda1731@gmail.com>
2020-06-05 16:27:56 +0900

    vcode test

--- functions.php
+++ functions.php
@@ -1,13 +1,14 @@
 <?php
 
-function connect_to_db(){    
-  $dbn='mysql:dbname=gsacf_l03_02;charset=utf8;port=3306;host=localhost';
-     $user = 'root';
-         $pwd = '';
-            try {        
-              return new PDO($dbn, $user, $pwd);   
-             } catch (PDOException $e) {        
-               exit('dbError:'.$e->getMessage());   
-               } 
-               }
- 
+//aaaaa
+function connect_to_db()
+{
+  $dbn = 'mysql:dbname=gsacf_l03_02;charset=utf8;port=3306;host=localhost';
+  $user = 'root';
+  $pwd = '';
+  try {
+    return new PDO($dbn, $user, $pwd);
+  } catch (PDOException $e) {
+    exit('dbError:' . $e->getMessage());
+  }
+}
